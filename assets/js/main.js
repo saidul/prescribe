fdc6e34b0ad963a9ac764e8fcfad0829fb5c507f
Saidul Islam <saidul.04@gmail.com>
2012-07-26 23:50:40 +0600

    update...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
- 
+(function(window, $, document){
+var Prescription = {
+    init: function(){
+        Prescription.addNewOeFeature();
+    },
+
+    addNewOeFeature: function(){
+        $('#btn-new-oe').live('click', function(){
+               $('#modal-oe').modal('show');
+        });
+    }
+}
+
+$(document).ready(function(){
+    Prescription.init();
+})
+})(window, jQuery, document);
