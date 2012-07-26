fff76328c805d03d9cbe67cb63950a7cbc0242fd
Saidul Islam <saidul.04@gmail.com>
2012-07-27 00:05:01 +0600

    dynamic class added when printing

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,22 @@
 var Prescription = {
     init: function(){
         Prescription.addNewOeFeature();
+
+
+
+
+        Prescription.addPrintFeature();
     },
 
+    addPrintFeature: function(){
+        $('#print').on('click', function(){
+            $('body').addClass('printing');
+
+            window.print();
+
+            $('body').removeClass('printing');
+        });
+    },
     addNewOeFeature: function(){
         $('#btn-new-oe').live('click', function(){
                $('#modal-oe').modal('show');
