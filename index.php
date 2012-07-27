49ed96c09139798c6fe44725940c998ec90b27d0
Saidul Islam <saidul.04@gmail.com>
2012-07-27 20:28:55 +0600

    update..

--- index.php
+++ index.php
@@ -6,6 +6,23 @@ $app['debug'] = true;
 $app['data-source'] = "sqlite:".__DIR__.'/data.db3';    //PDO connection string
 
 
+$app['insert-map'] = array(
+    'chief_complain'     => 'INSERT INTO "main"."chief_complain" ( "id", "name") values (:id, :name);',
+    'onsite_examination' => 'INSERT INTO "main"."onsite_examination" ( "id" , "name", "shortName" , "unit" ) values (:id, :name, :shortName, :unit);',
+    'tests'              => 'INSERT INTO "main"."tests" ( "id" , "name" ) values (:id, :name);',
+    'advice'             => 'INSERT INTO "main"."advice" ( "id" , "text" , "useCount" ) values (:id, :text, :useCount);',
+    'comments'           => 'INSERT INTO "main"."comments" ( "id" , "comment" , "cType" , "fKey" ) values (:id, :comment, :cType, :fKey);',
+    'prescription'       => 'INSERT INTO "main"."prescription" ( "id" , "name" , "age" , "date" , "parent" , "data" ) values (:id, :name, :age, :date, :parent, :data);',
+);
+
+$app['table-map'] = array(
+    'cc'       => 'chief_complain',
+    'oe'       => 'onsite_examination',
+    'tests'    => 'tests',
+    'advice'   => 'advice',
+    'comments' => 'comments',
+);
+
 
 $app->before(function () use ($app){
     $app['dbh'] = new PDO($app['data-source']);
@@ -53,73 +70,113 @@ $app->post('/sync', function() use ($app){
 
     $request = json_decode($app['request']->request->get('request'),true);
 
-    $tableMap = array(
-        'cc'       => 'chief_complain',
-        'oe'       => 'onsite_examination',
-        'tests'    => 'tests',
-        'advice'   => 'advice',
-        'comments' => 'comments',
-    );
 
 
-    $tables = array(
-        'chief_complain'     => 'INSERT INTO "main"."chief_complain" ( "id", "name") values (?, ?);',
-        'onsite_examination' => 'INSERT INTO "main"."onsite_examination" ( "id" , "name", "shortName" , "unit" ) values (?, ?, ?, ?);',
-        'tests'              => 'INSERT INTO "main"."tests" ( "id" , "name" ) values (?, ?);',
-        'advice'             => 'INSERT INTO "main"."advice" ( "id" , "text" , "useCount" ) values (?, ?, ?);',
-        'comments'           => 'INSERT INTO "main"."comments" ( "id" , "comment" , "cType" , "fKey" ) values (?, ?, ?, ?);',
-        'prescription'       => 'INSERT INTO "main"."prescription" ( "id" , "name" , "age" , "date" , "parent" , "data" ) values (?, ?, ?, ?, ?, ?);',
-    );
+     $loadComments = function($cType, $fKey, $comments) use ($app) {
+            foreach($comments as $comment) {
+                $id = is_exist('comments', $comment, $app);
+                if(false === $id) {
+                    $stmt = $app['dbh']->prepare($app['insert-map']['comments']);
+                    $stmt->execute(array_merge($comment, array('cType' => $cType, 'fKey' => $fKey)));
+                }
+            }
+     };
+
+
+
+    //$recordExist
 
      foreach($request['data'] as $tabKey => $rows) {
-        if(!isset($tableMap[$tabKey])) continue;
-         $tableName = $tableMap[$tabKey];
-         $stmt = $app['dbh']->query($tables[$tableName]);
+        if(!isset($app['table-map'][$tabKey])) continue; //Skip not mapped tables
+
+         $tableName = $app['table-map'][$tabKey];
+         $stmt = $app['dbh']->prepare($app['insert-map'][$tableName]);
          foreach($rows as $row)
          {
              //TODO: if exist then skip else insert
-             $stmt->execute($row);
+             $comments = isset($row['comments']) ? $row['comments'] : array();
+             unset($row['comments']);
+             $id = is_exist($tableName, $row, $app);
+             if(false === $id){
+               $stmt->execute($row);
+             } else {
+                 $row['id'] = $id;
+             }
+
+
+             $loadComments($tableName, $row['id'], $comments);
          }
      }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    echo "<pre>";
-    // Drop databases
-    foreach($tables as $tableName => $sql) {
-
-    }
-
-    echo "</pre>";
 });
 
 
-$app->get('/getData/all', function() use ($app){
-    $tableName = 'chief_complain';
+$app->get('/getData/{type}', function($type) use ($app){
+
+    if(!isset($app['table-map'][$type])) $app->abort(500, "Unknown type");
+
+    $tableName = $app['table-map'][$type];
     $sql = "SELECT * from {$tableName};";
     /** @var $statement PDOStatement */
     $statement = $app['dbh']->query($sql);
     $statement->setFetchMode(PDO::FETCH_ASSOC);
+    $data = array();
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $comments = getComments($tableName, $row['id'], $app);
+        if(count($comments))
+            $row['comments'] = $comments;
 
-    return $app->json($statement->fetchAll());
+        $data[] = $row;
+    }
+
+    return $app->json($data);
 
 });
 
 
-
-
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
-});
 $app->run();
 
+
+function getComments($cType, $fKey, $app){
+    $sql = "SELECT * FROM comments WHERE cType = :cType AND fKey = :fKey";
+    /** @var $stmt PDOStatement */
+    $stmt = $app['dbh']->prepare($sql);
+    $stmt->bindValue(':cType', $cType);
+    $stmt->bindValue(':fKey', $fKey);
+
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+
+function is_exist($table, $row, $app) {
+    $searchField = array(
+        'chief_complain'     => 'name',
+        'onsite_examination' => 'shortName',
+        'tests'              => 'name',
+        'advice'             => 'text',
+        'comments'           => 'comment'
+    );
+
+    if($table == 'comments') {
+        $sql = "SELECT id, COUNT(id) as totalMatch FROM $table WHERE comment LIKE :comment AND cType = :cType AND fKey = :fKey";
+        /** @var $stmt PDOStatement */
+        $stmt = $app['dbh']->prepare($sql);
+        $stmt->bindValue(':comment', $row['comment']);
+        $stmt->bindValue(':cType', $row['cType']);
+        $stmt->bindValue(':fKey', $row['fKey']);
+    } else {
+        $colName = $searchField[$table];
+        $sql = "SELECT id, COUNT($colName) as totalMatch FROM $table WHERE $colName LIKE :$colName";
+        /** @var $stmt PDOStatement */
+        $stmt = $app['dbh']->prepare($sql);
+        $stmt->bindValue(':'.$colName, $row[$colName]);
+    }
+    $stmt->execute();
+
+    $data = $stmt->fetch();
+
+    return $data['totalMatch'] > 0 ? $data['id'] : false;
+
+}
